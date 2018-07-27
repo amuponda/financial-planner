e0a8419fa888184725fa92fbac54c755cbfaafdd
Alfred Muponda <alfredmuponda@gmail.com>
2018-07-28 08:08:07 +1000

    Add more client dependencies

--- client/src/main.js
+++ client/src/main.js
@@ -4,13 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import * as uiv from 'uiv'
+import VeeValidate from 'vee-validate'
+import BootstrapVue from 'bootstrap-vue'
 import './assets/css/bootstrap.css'
 import './assets/css/grails.css'
 import './assets/css/main.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 Vue.use(uiv)
+Vue.use(VeeValidate)
+Vue.use(BootstrapVue)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
