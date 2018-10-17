6fc4bc7a521f632cc0d69d7d72d1e0feb94bdfc9
Alfred Muponda <alfredmuponda@gmail.com>
2018-10-18 02:12:34 +1000

    Assemble the client and server into one jar for release in production

--- server/grails-app/controllers/com/financial/planner/UrlMappings.groovy
+++ server/grails-app/controllers/com/financial/planner/UrlMappings.groovy
@@ -1,11 +1,17 @@
 package com.financial.planner
 
+import grails.util.Environment
+
 class UrlMappings {
 
     static mappings = {
+        if ( Environment.current == Environment.PRODUCTION ) {
+            '/'(uri: '/index.html')
+        } else {
+            '/'(controller: 'application', action:'index')
+        }
         "/api/signup"(controller: 'user', action: 'signup')
         "/api/user"(controller: 'user', action: 'index')
-        "/"(controller: 'application', action:'index')
         "500"(view: '/error')
         "404"(view: '/notFound')
     }
