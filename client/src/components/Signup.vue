c1ee7a928431796082d8fd5a439e9561197f0a15
Alfred Muponda <alfredmuponda@gmail.com>
2018-11-24 15:36:41 +1000

    Switch the signup form to pure bootstrap

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -1,42 +1,43 @@
 <template>
-  <b-container>
-    <b-row>
-      <b-col sm="4" offset-sm="4">
-        <b-card border-variant="primary">
+  <div class="container mt-5">
+    <div class="row justify-content-md-center">
+      <div class="col-sm-4">
+        <div class="card text-center border-0">
+          <div class="card-body">
+            <h4 class="card-title text-center">Sign Up</h4>
 
-          <h4 slot="header" class="text-center">Sign Up</h4>
+            <div v-if="errors">
+              <span v-for="error in errors" :key="error.message" class="text-danger">{{ error.message }}</span>
+            </div>
 
-          <div v-if="errors">
-            <span v-for="error in errors" :key="error.message" class="text-danger">{{ error.message }}</span>
+            <form @submit.prevent="submit" novalidate>
+              <div class="form-group">
+                <input v-model="form.firstName" name="firstName" v-validate="'required'" class="form-control" placeholder="Name"/>
+                <p v-show="errors.has('firstName')" class="text-danger">{{ errors.first('firstName') }}</p>
+              </div>
+
+              <div class="form-group">
+                <input v-model="form.lastName" name="lastName" v-validate="'required'" class="form-control" placeholder="Surname"/>
+                <p v-show="errors.has('lastName')" class="text-danger">{{ errors.first('lastName') }}</p>
+              </div>
+
+              <div class="form-group">
+                <input type="email" v-model="form.username" name="username" v-validate="'required|email'" class="form-control" placeholder="Email"/>
+                <p v-show="errors.has('username')" class="text-danger">{{ errors.first('username') }}</p>
+              </div>
+
+              <div class="form-group">
+                <input type="password" v-model="form.password" name="password" v-validate="'required'" class="form-control" placeholder="Password"/>
+                <p v-show="errors.has('password')" class="text-danger">{{ errors.first('password') }}</p>
+              </div>
+
+              <button type="submit" class="btn btn-primary btn-sm px-5">Sign Up</button>
+            </form>
           </div>
-
-          <b-form @submit.prevent="submit" novalidate>
-            <b-form-group id="nameGroup" label="Name:" label-for="name">
-              <b-form-input id="name" type="text" v-model="form.firstName" name="firstName" v-validate="'required'"></b-form-input>
-              <p v-show="errors.has('firstName')" class="text-danger">{{ errors.first('firstName') }}</p>
-            </b-form-group>
-
-            <b-form-group id="surnameGroup" label="Surname:" label-for="surname">
-              <b-form-input id="surname" type="text" v-model="form.lastName" name="lastName" v-validate="'required'"></b-form-input>
-              <p v-show="errors.has('lastName')" class="text-danger">{{ errors.first('lastName') }}</p>
-            </b-form-group>
-
-            <b-form-group id="emailGroup" label="Email:" label-for="email">
-              <b-form-input id="email" type="email" v-model="form.username" name="username" v-validate="'required|email'"></b-form-input>
-              <p v-show="errors.has('username')" class="text-danger">{{ errors.first('username') }}</p>
-            </b-form-group>
-
-            <b-form-group id="passwordGroup" label="Password:" label-for="password">
-              <b-form-input id="password" type="password" v-model="form.password" name="password" v-validate="'required'"></b-form-input>
-              <p v-show="errors.has('password')" class="text-danger">{{ errors.first('password') }}</p>
-            </b-form-group>
-
-            <b-button type="submit" variant="primary">Sign Up</b-button>
-          </b-form>
-        </b-card>
-      </b-col>
-    </b-row>
-  </b-container>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -72,7 +73,7 @@ export default {
                 this.$router.push({ name: 'login' })
                 return
               }
-              this.errors = response._embedded.errors
+              this.errors = response.data.errors
             })
             .catch(error => {
               this.errors = [{ message: error.message }]
@@ -85,7 +86,7 @@ export default {
 </script>
 
 <style scoped>
- .card {
-   margin-top: 25%;
- }
+  .form-control {
+    height: calc(1.25rem + 14px);
+  }
 </style>
