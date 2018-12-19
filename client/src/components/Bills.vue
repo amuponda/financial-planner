5cbdd364fbeccc585b9932633570ea69f7809b70
Alfred Muponda <alfredmuponda@gmail.com>
2018-12-20 03:17:15 +1000

    Show list of bills on the Bills page

--- client/src/components/Bills.vue
+++ client/src/components/Bills.vue
@@ -1,23 +1,19 @@
 <template>
   <div class="container">
     <div class="row mt-5">
-      <div class="col-sm-4">
+      <div class="col-md-4">
         <!-- bills summary -->
         <div class="card">
           <div class="card-body">
             <h5 class="card-title">Summary</h5>
             <div>
-              <small>
-                <strong>Monthly Total:&nbsp;</strong>
-              </small>
+              <strong>Monthly Total:&nbsp;</strong>
               <strong>$0.00</strong><br>
-              <small>
-                <strong>Month To Date:&nbsp;</strong>
-              </small>
+
+              <strong>Month To Date:&nbsp;</strong>
               <strong>$0.00</strong><br>
-              <small>
-                <strong>Month Remaining:&nbsp;</strong>
-              </small>
+
+              <strong>Month Remaining:&nbsp;</strong>
               <strong>$0.00</strong>
             </div>
             <div class="mt-3">
@@ -28,19 +24,25 @@
       </div>
 
       <!--Bills list -->
-      <div class="col-sm-8">
+      <div class="col-md-8">
         <div class="card">
-          <div class="card-body">
-            <div class="card-title">Bills</div>
-            <table v-if="bills">
+          <div class="card-body table-responsive">
+            <h5 class="card-title">Bills</h5>
+            <table v-if="bills" class="table table-striped table-sm">
               <thead>
               <tr>
+                <th>Since</th>
                 <th>Name</th>
+                <th>Amount</th>
+                <th>Category</th>
               </tr>
               </thead>
               <tbody>
               <tr v-for="bill in bills" :key="bill.id">
+                <td>{{ bill.startDate | date }}</td>
                 <td v-text="bill.name"></td>
+                <td>{{ bill.amount | currency }}</td>
+                <td>{{ bill.category | enumPrettify(categories) }}</td>
               </tr>
               </tbody>
             </table>
@@ -57,6 +59,7 @@
 import { mapGetters } from 'vuex'
 import CreateBillModal from './CreateBillModal'
 import modalMixin from '../mixins/ModalMixin'
+import dayjs from 'dayjs'
 
 export default {
   name: 'Bills',
@@ -64,9 +67,20 @@ export default {
   computed: mapGetters({
     accounts: 'getAccounts',
     bills: 'getBills',
-    isInitBills: 'isInitBills'
+    isInitBills: 'isInitBills',
+    categories: 'getCategories'
   }),
   components: { CreateBillModal },
+  filters: {
+    date (value) {
+      if (!value) return '-'
+      return dayjs(value).format('DD/MM/YYYY')
+    },
+    enumPrettify (value, enums) {
+      let pretty = enums.find(e => e.name === value)
+      return pretty ? pretty.label : value
+    }
+  },
   created () {
     if (!this.isInitBills) {
       this.$store.dispatch('fetchBills')
