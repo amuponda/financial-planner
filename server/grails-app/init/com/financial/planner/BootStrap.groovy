327da8231bae4e4d01d2814b3f98a40b0744bd38
Alfred Muponda <alfredmuponda@gmail.com>
2019-01-22 02:35:07 +1000

    Create default user ROLE on startup if it does not exist

--- server/grails-app/init/com/financial/planner/BootStrap.groovy
+++ server/grails-app/init/com/financial/planner/BootStrap.groovy
@@ -3,7 +3,9 @@ package com.financial.planner
 class BootStrap {
 
     def init = { servletContext ->
-
+        if (!Role.findByAuthority('ROLE_USER')) {
+            new Role(authority: 'ROLE_USER').save(flush: true)
+        }
     }
 
     def destroy = {
